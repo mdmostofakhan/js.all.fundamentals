db4d6098b3cbb6fb3099abe0c69685d5fe70adec
mostofa khan <mdmostofakhan1972@gmail.com>
2024-03-06 18:13:22 +0600

    first

---
+++ error.js
@@ -0,0 +1,40 @@
+try{
+    addlert("welcom ")
+} catch(err){
+    // console.log(err.message)
+}
+
+let x = 4;
+try{
+    if( x == '') throw "emt";
+    if(isNaN(x)) throw "not a number"
+    x = Number(x)
+    if(x < 5) throw "this is a low";
+    if(x > 10) throw {
+        name: "too heigh",
+        lastName: "this is a message"
+    }
+}
+ catch(err){
+    // console.log(err)
+ }
+
+// console.log("thi is a test")
+
+
+let num = 1;
+try {
+  num.toPrecision(500);   // A number cannot have 500 significant digits
+}
+catch(err) {
+   console.log(err.name)
+}
+
+
+let m = 5;
+try {
+  m = y + 1;   // y cannot be used (referenced)
+}
+catch(err) {
+  console.log(err.name)
+}
---
+++ regularEx.js
@@ -0,0 +1,34 @@
+// let text = "Visit my website"
+// let n = text.search("my")
+// console.log(n)
+
+// let repl = "this is my website microsoft "
+// let result = repl.replace("microsoft", "you tube channel")
+// console.log(result)
+
+// let outPut = "this  microsoft "
+// let results = outPut.replace(/microsoft/ig, "how funny man")
+// console.log(results)
+
+
+// let counts = "this Hellooo hello w3school  microsoft "
+// let countFor = outPut.match(/lo*/g)
+// console.log(countFor)
+
+// const pattern = /o/;
+// const patternTotal = pattern.test("The best things in life are free!");
+// console.log(patternTotal)
+
+
+let textSearch = "Hello Microsoft word"
+let textSearchOn = textSearch.search("Microsoft", "Messengers")
+console.log(textSearchOn)
+
+let textMatch = "Hello Microsoft word";
+let textMatchOn = textMatch.match(/lo*/ig)
+console.log(textMatchOn)
+
+let textReplace = "Hello Microsoft word";
+let replaceText = textReplace.replace("Microsoft", "JavaScript Regular")
+console.log(replaceText) 
+
