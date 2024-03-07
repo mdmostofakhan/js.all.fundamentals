18ac999635efd26ec9f44527b15ebb3c24fbae1d
mostofa khan <mdmostofakhan1972@gmail.com>
2024-03-07 13:01:07 +0600

    seond

---
+++ json.js
@@ -0,0 +1,10 @@
+
+
+const person = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+const obj = JSON.parse(person)
+console.log(obj)
+
+console.log('222')
